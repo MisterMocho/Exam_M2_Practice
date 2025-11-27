1eb8c4cf3646dcd844e1abc85d66575cd218f4d6
MisterMocho <lmcp95@gmail.com>
2025-11-27 07:10:31 +0000

    Refactor flood_fill function for clarity

--- level_3/flood_fill.c
+++ level_3/flood_fill.c
@@ -1,11 +1,5 @@
 #include "flood_fill.h"
 
-void	flood_fill(char **tab, t_point size, t_point begin)
-{
-	char	target = tab[begin.y][begin.x];
-	fill(tab, size, target, begin.y, begin.x);
-}
-
 void	fill(char **tab, t_point size, char target, int row, int col)
 {
 	if (row < 0 || col < 0 || row >= size.y || col >= size.x)
@@ -17,4 +11,10 @@ void	fill(char **tab, t_point size, char target, int row, int col)
 	fill(tab, size, target, row + 1, col);
 	fill(tab, size, target, row, col - 1);
 	fill(tab, size, target, row, col + 1);
+}
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	char	target = tab[begin.y][begin.x];
+	fill(tab, size, target, begin.y, begin.x);
 }
