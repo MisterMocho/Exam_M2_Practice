1c2bb5be6e4a24745ee9e6b2dc0773278f42d03a
MisterMocho <lmcp95@gmail.com>
2025-11-27 06:38:49 +0000

    Refactor main function for argument handling
    Refactored main function to handle both all arguments and just the first argument. Added logic to manage spaces and output accordingly.

--- level_3/rostring.c
+++ level_3/rostring.c
@@ -6,7 +6,7 @@ int	is_space(char c)
 		return (1);
 	return (0);
 }
-
+// subject is a bit ambiguous, so, my solution to apply it on all the arguments
 int	main(int argc, char **argv)
 {
 	int	i;
@@ -26,6 +26,8 @@ int	main(int argc, char **argv)
 	{
 		i = 0;
 		fw = 1;
+		s = 0;
+		e = 0;
 		while (argv[j][i])
 		{
 			if (is_space(argv[j][i]))
@@ -49,9 +51,57 @@ int	main(int argc, char **argv)
 				}
 			}
 		}
-		write(1, &argv[j][s], e - s);
+		if (e > s)
+			write(1, &argv[j][s], e - s);
 		write(1, "\n", 1);
 		j++;
 	}
 	return (0);
 }
+
+// solution for just the first argument and ignoring all extra args:
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	s;
+	int	e;
+	int fw;
+	
+	if (argc < 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	i = 0;
+	fw = 1;
+	s = 0;
+	e = 0;
+	while (argv[1][i])
+	{
+		if (is_space(argv[1][i]))
+			i++;
+		else if (!is_space(argv[1][i]))
+		{
+			if (fw == 1)
+				s = i;
+			while (argv[1][i] && !is_space(argv[1][i]))
+			{
+				if (fw == 0)
+					write(1, &argv[1][i], 1);
+				i++;	
+			}
+			if (fw == 0)
+				write(1, " ", 1);
+			if (fw == 1)
+			{	
+				e = i;
+				fw = 0;
+			}
+		}
+	}
+	if (e > s)
+		write(1, &argv[1][s], e - s);
+	write(1, "\n", 1);
+	return (0);
+}
+
