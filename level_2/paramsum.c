dc9a75c01857507171edb032fd86dcb31365d6f7
MisterMocho <lmcp95@gmail.com>
2025-11-27 04:54:57 +0000

    Fix ft_putnbr to correctly print number of arguments

--- level_2/paramsum.c
+++ level_2/paramsum.c
@@ -4,13 +4,14 @@ void	ft_putnbr(long n)
 {
 	if (n >= 10)
 		ft_putnbr(n / 10);
-	write(1, &(char){(n - 1) + '0'}, 1);
+	write(1, &(char){(n % 10) + '0'}, 1);
 }
 
 int	main(int argc, char **argv)
 {
+	(void)argv;
 	if (argc >= 1)
-		ft_putnbr((long)argc);
+		ft_putnbr((long)argc - 1);
 	write(1, "\n", 1);
 	return (0);
 }
