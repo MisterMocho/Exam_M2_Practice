8be043931fd78098862ea28be4d189802c7bcf6d
MisterMocho <lmcp95@gmail.com>
2025-11-27 05:08:06 +0000

    Handle empty input case in hidenp.c

--- level_2/hidenp.c
+++ level_2/hidenp.c
@@ -12,6 +12,11 @@ int	main(int argc, char **argv)
 		write(1, "\n", 1);
 		return (0);
 	}
+	if (argv[1][0] == '\0')
+    {
+        write(1, "1\n", 2);
+        return (0);
+    }
 	while (argv[2][j])
 	{
 		if (argv[1][i] == argv[2][j])
