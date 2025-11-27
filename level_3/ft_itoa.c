6300b32d77092b1978894ebfb6ef2be5706daa9f
MisterMocho <lmcp95@gmail.com>
2025-11-27 07:21:33 +0000

    Fix issues in ft_itoa function implementation

--- level_3/ft_itoa.c
+++ level_3/ft_itoa.c
@@ -5,11 +5,11 @@ int	ft_intlen(long int i)
 	int len;
 
 	len = 0;
-	if (i <= 0);
+	if (i <= 0)
 		len = 1;
 	while (i)
 	{
-		i / 10;
+		i /= 10;
 		len++;
 	}
 	return (len);
@@ -22,13 +22,13 @@ char	*ft_itoa(int nbr)
 	unsigned int	len;
 
 	i = nbr;
-	len = ft_intlen(i) + 1;
-	str = malloc(sizeof(char) * (len));
+	len = ft_intlen(i);
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	str[len] = '\0';
 	if (i == 0)
-		str[0] == '0';
+		str[0] = '0';
 	else if (i < 0)
 	{
 		str[0] = '-';
